9b9ddb756bb40d9b387fa4f8e43dcb8140a6dc9e
Rafael Bodill <justrafi@gmail.com>
2016-11-12 20:03:40 +0200

    Add helper script to create & update virtual-envs

---
+++ venv.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Declare a base path for both virtual environments
+venv="${XDG_CACHE_HOME:-$HOME/.cache}/vim/venv"
+
+# Try to detect virtualenv's executable names
+vrenv2=virtualenv2
+hash virtualenv-2.7 2>/dev/null && vrenv2=virtualenv-2.7
+vrenv3=virtualenv3
+hash virtualenv-3.4 2>/dev/null && vrenv3=virtualenv-3.4
+
+# Ensure python 2/3 virtual environments
+[ -d "$venv" ] || mkdir -p "$venv"
+[ -d "$venv/neovim2" ] || $vrenv2 "$venv/neovim2"
+[ -d "$venv/neovim3" ] || $vrenv3 "$venv/neovim3"
+
+# Install or upgrade dependencies
+"$venv/neovim2/bin/pip" install -U neovim PyYAML
+"$venv/neovim3/bin/pip" install -U neovim PyYAML
