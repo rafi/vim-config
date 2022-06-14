421dfd36fc1529ae452d9ee65ce697295fdac738
thecontinium <thecontinium@outlook.com>
2022-06-14 11:44:08 +0100

    added migration

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -4,42 +4,6 @@
 local winwidth = 25
 
 -- :h nvim-tree.lua
-vim.g.nvim_tree_group_empty = 1
-vim.g.nvim_tree_respect_buf_cwd = 1
-
-vim.g.nvim_tree_git_hl = 0
-vim.g.nvim_tree_highlight_opened_files = 0
-vim.g.nvim_tree_indent_markers = 1
-vim.g.nvim_tree_add_trailing = 1
-vim.g.nvim_tree_create_in_closed_folder = 0
-
-vim.g.nvim_tree_icon_padding = vim.g.global_symbol_padding or ' '
-vim.g.nvim_tree_special_files = {}
-vim.g.nvim_tree_show_icons = {
-	git = 1, folders = 1, files = 1, folder_arrows = 0 }
-vim.g.nvim_tree_icons = {
-	default = '',
-	symlink = '',
-	git = {
-		unstaged = '+',
-		staged = '+',
-		unmerged = '',
-		renamed = '',
-		untracked = '?',
-		deleted = '',
-		ignored = '!'
-	},
-	folder = {
-		arrow_open = '',
-		arrow_closed = '',
-		default = '',
-		open = '',
-		empty = '',
-		empty_open = '',
-		symlink = '',
-		symlink_open = '',
-	}
-}
 
 -- Custom actions
 
@@ -88,6 +52,8 @@ end
 
 -- :h nvim-tree
 require('nvim-tree').setup({
+	create_in_closed_folder = false,
+  respect_buf_cwd = true,
 	hijack_netrw = true,
 	hijack_directories = {
 		enable = false,
@@ -134,6 +100,48 @@ require('nvim-tree').setup({
 				},
 			}
 		}
+	},
+	renderer = {
+		add_trailing = true,
+		group_empty = true,
+    highlight_git = false,
+    highlight_opened_files = "none",
+		indent_markers = {
+			enable = true,
+		},
+		icons = {
+			padding = vim.g.global_symbol_padding or ' ',
+			show = {
+				file = true,
+				folder = true,
+				folder_arrow = false,
+				git = true,
+			},
+			glyphs = {
+				default = '',
+				symlink = '',
+				git = {
+					unstaged = '+',
+					staged = '+',
+					unmerged = '',
+					renamed = '',
+					untracked = '?',
+					deleted = '',
+					ignored = '!'
+				},
+				folder = {
+					arrow_open = '',
+					arrow_closed = '',
+					default = '',
+					open = '',
+					empty = '',
+					empty_open = '',
+					symlink = '',
+					symlink_open = '',
+				}
+		  },
+		},
+		special_files = {},
 	},
 	view = {
 		width = winwidth,
