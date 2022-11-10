0ea42bcc1ebbfb02151487441310bd44f65268d9
Aleksandr Palyan <a.palyan@4irelabs.com>
2022-11-10 10:13:42 +0200

    move config for registers plugin to lua/plugins (required by new version)

---
+++ lua/plugins/registers.lua
@@ -0,0 +1,11 @@
+-- plugin: registers
+-- see: https://github.com/tversteeg/registers.nvim
+-- rafi settings
+
+local registers = require("registers")
+
+registers.setup({
+  window = {
+    border = "rounded"
+  }
+})
