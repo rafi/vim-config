4c5070260520de7763f96f38288692910bd31d28
Aleksandr Palyan <a.palyan@4irelabs.com>
2022-11-23 09:47:01 +0200

    automatic formatting code when buffer save

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -96,6 +96,19 @@ local on_attach = function(client, bufnr)
 			augroup END
 		]], false)
 	end
+
+	if client.supports_method("textDocument/formatting") then
+		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+				-- vim.lsp.buf.formatting_sync()
+				vim.lsp.buf.format({ bufnr = bufnr })
+			end,
+		})
+	end
 end
 
 -- Combine base config for each server and merge user-defined settings.
