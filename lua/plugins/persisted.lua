59baa57c73ba60b099ebd6a22e746699eb248f5b
loadedice <loadedice@users.noreply.github.com>
2023-02-26 15:26:14 +1100

    replace deprecated persisted callbacks with user events

--- lua/plugins/persisted.lua
+++ lua/plugins/persisted.lua
@@ -16,19 +16,32 @@ persisted.setup({
 	follow_cwd = false,
 	use_git_branch = false,
 	ignored_dirs = { '/tmp', '~/.cache' },
+})
 
-	before_save = function()
+local group = vim.api.nvim_create_augroup("PersistedHooks", {})
+
+vim.api.nvim_create_autocmd({ "User" }, {
+  pattern = "PersistedSavePre",
+  group = group,
+  callback = function()
 		require('interface').win.close_plugin_owned()
-	end,
+  end,
+})
 
-	telescope = {
-		before_source = function()
-	 		persisted.save()
-			persisted.stop()
-		end,
-		after_source = function(session)
-			persisted.start()
-			print('Started session ' .. session.name)
-		end
-	}
+vim.api.nvim_create_autocmd({ "User" }, {
+  pattern = "PersistedTelescopeLoadPre",
+  group = group,
+  callback = function()
+		persisted.save()
+		persisted.stop()
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "User" }, {
+  pattern = "PersistedTelescopeLoadPre",
+  group = group,
+  callback = function(session)
+		persisted.start()
+		print('Started session ' .. session.name)
+  end,
 })
