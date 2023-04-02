bf1ddd7e21656a18442d534a92534af22c0adacb
Rafael Bodill <justrafi@gmail.com>
2023-04-02 21:19:26 +0300

    Improve noice routes and messages

--- lua/rafi/plugins/ui.lua
+++ lua/rafi/plugins/ui.lua
@@ -45,10 +45,57 @@ return {
 					['cmp.entry.get_documentation'] = true,
 				},
 			},
+			messages = {
+				view_search = false,
+			},
+			routes = {
+				{
+					filter = { event = 'msg_show', find = '%d+L, %d+B' },
+					view = 'mini',
+				},
+				{
+					filter = { event = 'msg_show', find = 'Hunk %d+ of %d+' },
+					view = 'mini',
+				},
+				{
+					filter = { event = 'msg_show', find = '%d+ more lines' },
+					opts = { skip = true },
+				},
+				{
+					filter = { event = 'msg_show', find = '%d+ lines yanked' },
+					opts = { skip = true },
+				},
+				{
+					filter = { event = 'msg_show', kind = 'quickfix' },
+					view = 'mini',
+				},
+				{
+					filter = { event = 'msg_show', kind = 'search_count' },
+					view = 'mini',
+				},
+				{
+					filter = { event = 'msg_show', kind = 'wmsg' },
+					view = 'mini',
+				},
+			},
 			presets = {
 				bottom_search = true,
 				command_palette = true,
 				long_message_to_split = true,
+			},
+			commands = {
+				all = {
+					view = 'split',
+					opts = { enter = true, format = 'details' },
+					filter = {},
+				},
+			},
+			---@type NoiceConfigViews
+			views = {
+				mini = {
+					zindex = 100,
+					win_options = { winblend = 0 },
+				},
 			},
 		},
 	},
@@ -133,18 +180,11 @@ return {
 		dependencies = 'lewis6991/gitsigns.nvim',
 		event = 'FileType',
 		keys = {
-			{ 'm/', '<cmd>MarksListAll<CR>', desc = 'Marks from all opened buffers'}
+			{ 'm/', '<cmd>MarksListAll<CR>', desc = 'Marks from all opened buffers' }
 		},
 		opts = {
-			default_mappings = true,
-			cyclic = true,
-			force_write_shada = false,
-			refresh_interval = 250,
 			sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20 },
-			bookmark_1 = {
-				sign = '',  -- ⚐ ⚑      
-				virt_text = '─────────────────────────────────',
-			},
+			bookmark_1 = { sign = '' },  -- ⚐ ⚑      
 			mappings = {
 				annotate = 'm<Space>',
 			},
