e009422aa12bcd7ee1934003670c677a00114be9
Rafael Bodill <justrafi@gmail.com>
2023-04-03 19:09:50 +0300

    feat(extras): introduce new extras.ui plugins

---
+++ lua/rafi/plugins/extras/ui/minimap.lua
@@ -0,0 +1,22 @@
+return {
+
+	{
+		'echasnovski/mini.map',
+		main = 'mini.map',
+		keys = {
+			{ '<Leader>mn', '<cmd>lua MiniMap.toggle()<CR>', desc = 'Mini map' },
+		},
+		opts = function()
+			local minimap = require('mini.map')
+			return {
+				integrations = {
+					minimap.gen_integration.diagnostic(),
+					minimap.gen_integration.builtin_search(),
+					minimap.gen_integration.gitsigns(),
+				},
+				window = { winblend = 50 }
+			}
+		end,
+	},
+
+}
