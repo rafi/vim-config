412316c08ed158208799c72cb4a6fcaa32a03e8b
Rafael Bodill <justrafi@gmail.com>
2023-04-05 19:48:48 +0300

    fix(diffview): update key-maps

--- lua/rafi/plugins/editor.lua
+++ lua/rafi/plugins/editor.lua
@@ -273,59 +273,43 @@ return {
 		keys = {
 			{ '<Leader>gv', '<cmd>DiffviewOpen<CR>', desc = 'Diff View' }
 		},
-		config = function()
+		opts = function()
 			local actions = require('diffview.actions')
-			vim.cmd [[
-				augroup rafi_diffview
-					autocmd!
-					autocmd WinEnter,BufEnter diffview://* setlocal cursorline
-					autocmd WinEnter,BufEnter diffview:///panels/* setlocal winhighlight=CursorLine:WildMenu
-				augroup END
-			]]
+			vim.api.nvim_create_autocmd({'WinEnter', 'BufEnter'}, {
+				group = vim.api.nvim_create_augroup('rafi_diffview', {}),
+				pattern = 'diffview:///panels/*',
+				callback = function()
+					vim.wo.winhighlight = 'CursorLine:WildMenu'
+				end,
+			})
 
-			require('diffview').setup({
+			return {
 				enhanced_diff_hl = true, -- See ':h diffview-config-enhanced_diff_hl'
 				keymaps = {
 					view = {
-						{'n', 'q',       '<cmd>DiffviewClose<CR>' },
-						{'n', '<tab>',   actions.select_next_entry },
-						{'n', '<s-tab>', actions.select_prev_entry },
-						{'n', ';a',      actions.focus_files },
-						{'n', ';e',      actions.toggle_files },
+						{ 'n', 'q',       '<cmd>DiffviewClose<CR>' },
+						{ 'n', '<Tab>',   actions.select_next_entry },
+						{ 'n', '<S-Tab>', actions.select_prev_entry },
+						{ 'n', '<LocalLeader>a', actions.focus_files },
+						{ 'n', '<LocalLeader>e', actions.toggle_files },
 					},
 					file_panel = {
-						{'n', 'q',       '<cmd>DiffviewClose<CR>' },
-						{'n', 'j',       actions.next_entry },
-						{'n', '<down>',  actions.next_entry },
-						{'n', 'k',       actions.prev_entry },
-						{'n', '<up>',    actions.prev_entry },
-						{'n', 'h',       actions.prev_entry },
-						{'n', 'l',       actions.select_entry },
-						{'n', '<cr>',    actions.select_entry },
-						{'n', 'o',       actions.focus_entry },
-						{'n', 'gf',      actions.goto_file },
-						{'n', 'sg',      actions.goto_file_split },
-						{'n', 'st',      actions.goto_file_tab },
-						{'n', 'r',       actions.refresh_files },
-						{'n', 'R',       actions.refresh_files },
-						{'n', '<c-r>',   actions.refresh_files },
-						{'n', '<tab>',   actions.select_next_entry },
-						{'n', '<s-tab>', actions.select_prev_entry },
-						{'n', ';a',      actions.focus_files },
-						{'n', ';e',      actions.toggle_files },
+						{'n', 'q',     '<cmd>DiffviewClose<CR>' },
+						{'n', 'h',     actions.prev_entry },
+						{'n', 'o',     actions.focus_entry },
+						{'n', 'gf',    actions.goto_file },
+						{'n', 'sg',    actions.goto_file_split },
+						{'n', 'st',    actions.goto_file_tab },
+						{'n', '<C-r>', actions.refresh_files },
+						{'n', ';e',    actions.toggle_files },
 					},
 					file_history_panel = {
-						{'n', 'o',    actions.focus_entry },
-						{'n', 'l',    actions.select_entry },
-						{'n', '<cr>', actions.select_entry },
-						{'n', 'O',    actions.options },
-					},
-					option_panel = {
-						{'n', '<tab>', actions.select },
-						{'n', 'q',     actions.close },
+						{'n', 'q', '<cmd>DiffviewClose<CR>' },
+						{'n', 'o', actions.focus_entry },
+						{'n', 'O', actions.options },
 					},
 				}
-			})
+			}
 		end
 	},
 
