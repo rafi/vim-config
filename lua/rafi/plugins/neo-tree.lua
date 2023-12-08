d45ed20cef2c56597aa0c7c4cde8a2fc3d9289a4
Rafael Bodill <justrafi@gmail.com>
2023-12-08 17:20:25 +0200

    feat(neo-tree): update keymaps and settings

--- lua/rafi/plugins/neo-tree.lua
+++ lua/rafi/plugins/neo-tree.lua
@@ -41,25 +41,65 @@ return {
 		's1n7ax/nvim-window-picker',
 	},
 	cmd = 'Neotree',
+	-- stylua: ignore
 	keys = {
 		{
-			'<LocalLeader>e',
-			'<cmd>Neotree filesystem left toggle dir=./<CR>',
-			desc = 'Explorer NeoTree Toggle',
+			'<leader>fe',
+			function()
+				local Util = require('lazyvim.util')
+				require('neo-tree.command').execute({ toggle = true, dir = Util.root() })
+			end,
+			desc = 'Explorer NeoTree (root dir)',
+		},
+		{
+			'<leader>fE',
+			function()
+				require('neo-tree.command').execute({ toggle = true, dir = vim.loop.cwd() })
+			end,
+			desc = 'Explorer NeoTree (cwd)',
 		},
 		{
 			'<LocalLeader>a',
-			'<cmd>Neotree filesystem left reveal dir=./<CR>',
+			function()
+				local Util = require('lazyvim.util')
+				require('neo-tree.command').execute({
+					reveal = true,
+					dir = Util.root()
+				})
+			end,
 			desc = 'Explorer NeoTree Reveal',
+		},
+		{ '<LocalLeader>e', '<leader>fe', desc = 'Explorer NeoTree (root dir)', remap = true },
+		{ '<leader>e', '<leader>fe', desc = 'Explorer NeoTree (root dir)', remap = true },
+		{ '<leader>E', '<leader>fE', desc = 'Explorer NeoTree (cwd)', remap = true },
+		{
+			'<leader>ge',
+			function()
+				require('neo-tree.command').execute({ source = 'git_status', toggle = true })
+			end,
+			desc = 'Git explorer',
+		},
+		{
+			'<leader>be',
+			function()
+				require('neo-tree.command').execute({ source = 'buffers', toggle = true })
+			end,
+			desc = 'Buffer explorer',
+		},
+		{
+			'<leader>xe',
+			function()
+				require('neo-tree.command').execute({ source = 'document_symbols', toggle = true })
+			end,
+			desc = 'Document explorer',
 		},
 	},
 	deactivate = function()
 		vim.cmd([[Neotree close]])
 	end,
 	init = function()
-		vim.g.neo_tree_remove_legacy_commands = 1
-		if vim.fn.argc() == 1 then
-			local stat = vim.loop.fs_stat(tostring(vim.fn.argv(0)))
+		if vim.fn.argc(-1) == 1 then
+			local stat = vim.loop.fs_stat(vim.fn.argv(0))
 			if stat and stat.type == 'directory' then
 				require('neo-tree')
 			end
@@ -68,6 +108,8 @@ return {
 	-- See: https://github.com/nvim-neo-tree/neo-tree.nvim
 	opts = {
 		close_if_last_window = true,
+		sources = { 'filesystem', 'buffers', 'git_status', 'document_symbols' },
+		open_files_do_not_replace_types = { 'terminal', 'Trouble', 'trouble', 'qf', 'Outline' },
 
 		source_selector = {
 			winbar = false,
@@ -85,14 +127,16 @@ return {
 			{
 				event = 'file_opened',
 				handler = function()
-					require('neo-tree').close_all()
+					require('neo-tree.command').execute({ action = 'close' })
 				end,
 			},
 		},
 
 		default_component_configs = {
 			indent = {
-				padding = 0,
+				expander_collapsed = '',
+				expander_expanded = '',
+				expander_highlight = 'NeoTreeExpander',
 			},
 			icon = {
 				folder_closed = '',
@@ -146,16 +190,6 @@ return {
 				['sg'] = 'open_vsplit',
 				['st'] = 'open_tabnew',
 
-				['c'] = { 'copy', config = { show_path = 'relative' } },
-				['m'] = { 'move', config = { show_path = 'relative' } },
-				['a'] = { 'add', nowait = true, config = { show_path = 'relative' } },
-				['N'] = { 'add_directory', config = { show_path = 'relative' } },
-				['d'] = 'noop',
-				['dd'] = 'delete',
-				['r'] = 'rename',
-				['y'] = 'copy_to_clipboard',
-				['x'] = 'cut_to_clipboard',
-				['P'] = 'paste_from_clipboard',
 				['<S-Tab>'] = 'prev_source',
 				['<Tab>'] = 'next_source',
 
@@ -164,13 +198,23 @@ return {
 					nowait = true,
 					config = { use_float = true },
 				},
-
 				['w'] = toggle_width,
 			},
 		},
 		filesystem = {
 			window = {
 				mappings = {
+					['d'] = 'noop',
+					['dd'] = 'delete',
+					['c'] = { 'copy', config = { show_path = 'relative' } },
+					['m'] = { 'move', config = { show_path = 'relative' } },
+					['a'] = { 'add', nowait = true, config = { show_path = 'relative' } },
+					['N'] = { 'add_directory', config = { show_path = 'relative' } },
+					['r'] = 'rename',
+					['y'] = 'copy_to_clipboard',
+					['x'] = 'cut_to_clipboard',
+					['P'] = 'paste_from_clipboard',
+
 					['H'] = 'toggle_hidden',
 					['/'] = 'noop',
 					['f'] = 'fuzzy_finder',
@@ -205,7 +249,7 @@ return {
 
 			filtered_items = {
 				visible = false,
-				show_hidden_count = true,
+				show_hidden_count = false,
 				hide_dotfiles = false,
 				hide_gitignored = false,
 				hide_by_name = {
@@ -235,5 +279,37 @@ return {
 				},
 			},
 		},
+		git_status = {
+			window = {
+				mappings = {
+					['d'] = 'noop',
+					['dd'] = 'delete',
+				},
+			},
+		},
+		document_symbols = {
+			follow_cursor = true,
+		},
 	},
+	config = function(_, opts)
+		local function on_move(data)
+			require('lazyvim.util').lsp.on_rename(data.source, data.destination)
+		end
+
+		local events = require('neo-tree.events')
+		opts.event_handlers = opts.event_handlers or {}
+		vim.list_extend(opts.event_handlers, {
+			{ event = events.FILE_MOVED, handler = on_move },
+			{ event = events.FILE_RENAMED, handler = on_move },
+		})
+		require('neo-tree').setup(opts)
+		vim.api.nvim_create_autocmd('TermClose', {
+			pattern = '*lazygit',
+			callback = function()
+				if package.loaded['neo-tree.sources.git_status'] then
+					require('neo-tree.sources.git_status').refresh()
+				end
+			end,
+		})
+	end,
 }
