28d67ebc88a979331824499e36446567b0f2d28e
Sergio Patino <sergio@vertigobox.org>
2024-01-09 01:05:47 -0700

    More config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,62 +1,78 @@
 return {
 
-  -- Enable GitHub's Copilot
-  { import = 'rafi.plugins.extras.coding.copilot' },
+	-- Enable GitHub's Copilot
+	{ import = "rafi.plugins.extras.coding.copilot" },
 
-  -- Accelerated jk
-  {
-    'rainbowhxch/accelerated-jk.nvim',
-    lazy = false,
-  },
+	-- Enable yaml-companion
+	{ import = "rafi.plugins.extras.lsp.yaml-companion" },
 
-  -- Waka Time coding tracker
-  {
-    'wakatime/vim-wakatime',
-    lazy = false,
-  },
+	-- Enable yaml plugins
+	{ import = "rafi.plugins.extras.lang.yaml" },
 
-  -- Github goto line
-  {
-    'ruanyl/vim-gh-line',
-    lazy = false,
-  },
+	-- Enable go plugins
+	{ import = "rafi.plugins.extras.lang.go" },
 
-  -- Change bufferline options
-  {
-    'akinsho/bufferline.nvim',
-    highlights = {
-      -- tab = {
-      --     fg = '<colour-value-here>',
-      --     bg = '<colour-value-here>',
-      -- },
-      -- tab_selected = {
-      --     fg = '<colour-value-here>',
-      --     bg = '<colour-value-here>',
-      -- },
-      -- tab_separator = {
-      --   fg = '<colour-value-here>',
-      --   bg = '<colour-value-here>',
-      -- },
-      -- tab_separator_selected = {
-      --   fg = '<colour-value-here>',
-      --   bg = '<colour-value-here>',
-      --   sp = '<colour-value-here>',
-      --   underline = '<colour-value-here>',
-      -- },
-    },
-    opts = {
-      options = {
-        indicator = {
-           style = 'underline',
-        },
-        show_tab_indicators = true,
-        style_preset = 'minimal',
-        separator_style = 'thin',
-        diagnostics_indicator = function(count, level, diagnostics_dict, context)
-          local icon = level:match("error") and " " or " "
-          return " " .. icon .. count
-        end,
-      }
-    }
-  },
+	-- Enable docker plugins
+	{ import = "rafi.plugins.extras.lang.docker" },
+
+	-- Enable python plugins
+	{ import = "rafi.plugins.extras.lang.python" },
+
+	-- Accelerated jk
+	{
+		"rainbowhxch/accelerated-jk.nvim",
+		lazy = false,
+	},
+
+	-- Waka Time coding tracker
+	{
+		"wakatime/vim-wakatime",
+		lazy = false,
+	},
+
+	-- Github goto line
+	{
+		"ruanyl/vim-gh-line",
+		lazy = false,
+	},
+
+	-- Change bufferline options
+	{
+		"akinsho/bufferline.nvim",
+		highlights = {
+			-- tab = {
+			--     fg = '<colour-value-here>',
+			--     bg = '<colour-value-here>',
+			-- },
+			-- tab_selected = {
+			--     fg = '<colour-value-here>',
+			--     bg = '<colour-value-here>',
+			-- },
+			-- tab_separator = {
+			--   fg = '<colour-value-here>',
+			--   bg = '<colour-value-here>',
+			-- },
+			-- tab_separator_selected = {
+			--   fg = '<colour-value-here>',
+			--   bg = '<colour-value-here>',
+			--   sp = '<colour-value-here>',
+			--   underline = '<colour-value-here>',
+			-- },
+		},
+		opts = {
+			options = {
+				indicator = {
+					style = "icon",
+					icon = "▎",
+				},
+				show_tab_indicators = true,
+				style_preset = "minimal",
+				separator_style = "thin",
+				diagnostics_indicator = function(count, level, diagnostics_dict, context)
+					local icon = level:match("error") and " " or " "
+					return " " .. icon .. count
+				end,
+			},
+		},
+	},
 }
