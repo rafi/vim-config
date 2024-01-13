214846787f3e2782500143e479969c8188d0f6ac
Rafael Bodill <justrafi@gmail.com>
2024-01-13 15:15:20 +0200

    feat(harpoon): use built-in telescope picker

--- lua/rafi/plugins/extras/editor/harpoon.lua
+++ lua/rafi/plugins/extras/editor/harpoon.lua
@@ -4,42 +4,26 @@ return {
 	{
 		'ThePrimeagen/harpoon',
 		branch = 'harpoon2',
-		-- stylua: ignore
 		opts = {},
+		-- stylua: ignore
 		keys = {
-			{ 'ga', function() require('harpoon'):list():append() end, desc = 'Add location' },
 			{ '<Leader>ua', 'ga', desc = 'Show character under cursor' },
+			{ 'ga', function() require('harpoon'):list():append() end, desc = 'Add location' },
+			{ '<C-n>', function() require('harpoon'):list():next() end, desc = 'Next location' },
+			{ '<C-p>', function() require('harpoon'):list():prev() end, desc = 'Previous location' },
+			{ '<Leader>mr', function() require('harpoon'):list():remove() end, desc = 'Remove location' },
 			{ '<LocalLeader>1', function() require('harpoon'):list():select(1) end, desc = 'Harpoon select 1' },
 			{ '<LocalLeader>2', function() require('harpoon'):list():select(2) end, desc = 'Harpoon select 2' },
 			{ '<LocalLeader>3', function() require('harpoon'):list():select(3) end, desc = 'Harpoon select 3' },
 			{ '<LocalLeader>4', function() require('harpoon'):list():select(4) end, desc = 'Harpoon select 4' },
-			{ 'gzp', function() require('harpoon'):list():prev() end, desc = 'Previous location' },
-			{ 'gzn', function() require('harpoon'):list():next() end, desc = 'Next location' },
 
-			-- { '<LocalLeader>l', function() require('harpoon').ui:toggle_quick_menu(require('harpoon'):list()) end, desc = 'Toggle Harpoon list' },
 			{ '<LocalLeader>l', function()
 				local harpoon = require('harpoon')
 				if not require('lazyvim.util').has('telescope.nvim') then
 					harpoon.ui:toggle_quick_menu(harpoon:list())
 					return
 				end
-				local conf = require('telescope.config').values
-				local function toggle_telescope(harpoon_files)
-					local file_paths = {}
-					for _, item in ipairs(harpoon_files.items) do
-						table.insert(file_paths, item.value)
-					end
-
-					require('telescope.pickers').new({}, {
-						prompt_title = 'Harpoon',
-						finder = require('telescope.finders').new_table({
-							results = file_paths,
-						}),
-						previewer = conf.file_previewer({}),
-						sorter = conf.generic_sorter({}),
-						}):find()
-				end
-				toggle_telescope(harpoon:list())
+				return require('telescope._extensions.marks')()
 			end, desc = 'List locations' },
 		},
 	},
