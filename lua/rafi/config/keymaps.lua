55f3bcd092ddf9555c3fe1ea145df110df5bc9b2
Rafael Bodill <justrafi@gmail.com>
2025-01-16 18:29:58 +0200

    feat(keymaps): saner behavior of n/N

--- lua/rafi/config/keymaps.lua
+++ lua/rafi/config/keymaps.lua
@@ -270,6 +270,14 @@ map('x', '<Leader>dd', '""Y""Pgv', { desc = 'Duplicate selection' })
 -- }}}
 -- Search, substitute, diff {{{
 
+-- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
+map('n', 'n', "'Nn'[v:searchforward].'zv'", { expr = true, desc = 'Next Search Result' })
+map('x', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next Search Result' })
+map('o', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next Search Result' })
+map('n', 'N', "'nN'[v:searchforward].'zv'", { expr = true, desc = 'Prev Search Result' })
+map('x', 'N', "'nN'[v:searchforward]", { expr = true, desc = 'Prev Search Result' })
+map('o', 'N', "'nN'[v:searchforward]", { expr = true, desc = 'Prev Search Result' })
+
 -- Switch */g* and #/g#
 map('n', '*', 'g*')
 map('n', 'g*', '*')
